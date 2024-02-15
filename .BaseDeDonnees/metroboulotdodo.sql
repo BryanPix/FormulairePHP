055a73177de0bbba050b91d473656f77f203d3e7
Festin bryan <festinbryan7@gmail.com>
2024-02-15 18:28:48 +0100

    Mise à jour  base de donnees dans le dossier

--- .BaseDeDonnees/metroboulotdodo.sql
+++ .BaseDeDonnees/metroboulotdodo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : jeu. 18 jan. 2024 à 15:56
+-- Généré le : jeu. 15 fév. 2024 à 17:18
 -- Version du serveur : 8.0.30
 -- Version de PHP : 8.1.10
 
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `metroboulotdodo`
 --
-CREATE DATABASE IF NOT EXISTS `metroboulotdodo` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `metroboulotdodo`;
 
 -- --------------------------------------------------------
 
@@ -94,7 +92,7 @@ CREATE TABLE IF NOT EXISTS `entreprise` (
   `ID_Entreprise` int NOT NULL AUTO_INCREMENT,
   `name_entreprise` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `email_entreprise` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `password_entreprise` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `password_entreprise` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `siretnumber_entreprise` varchar(14) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `adresse_entreprise` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `zipcode_entreprise` varchar(5) COLLATE utf8mb4_general_ci DEFAULT NULL,
@@ -102,15 +100,17 @@ CREATE TABLE IF NOT EXISTS `entreprise` (
   `Image_entreprise` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`ID_Entreprise`),
   UNIQUE KEY `siretnumber_entreprise` (`siretnumber_entreprise`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `entreprise`
 --
 
 INSERT INTO `entreprise` (`ID_Entreprise`, `name_entreprise`, `email_entreprise`, `password_entreprise`, `siretnumber_entreprise`, `adresse_entreprise`, `zipcode_entreprise`, `city_entreprise`, `Image_entreprise`) VALUES
-(1, 'LunaTech Solutions', 'LunaTech@gmail.com', 'kamini43', '28859301929402', '8 Rue de Rivoli', '75001', 'Paris', 'img.jpg'),
-(2, 'Phoenix Consulting and Design', 'PhoenixCo@yahoo.com', 'MarlyGomont23', '12279402390923', '25 avenue du Général de Gaulle', '69110', 'Lyon', 'img.jpg');
+(1, 'LunaTech Solutions', 'LunaTech@gmail.com', '$2y$10$h/fagMDp5ubBAPXGIpkoOOR71itZuX8lwNsRdL2ZkoFzIf/n85Nny', '28859301929402', '8 Rue de Rivoli', '75001', 'Paris', 'img.jpg'),
+(2, 'Phoenix Consulting and Design', 'PhoenixCo@yahoo.com', '$2y$10$h/fagMDp5ubBAPXGIpkoOOR71itZuX8lwNsRdL2ZkoFzIf/n85Nny', '12279402390923', '25 avenue du Général de Gaulle', '69110', 'Lyon', 'img.jpg'),
+(3, 'Balenciaga', 'Balenciaga@gmail.com', '$2y$10$h/fagMDp5ubBAPXGIpkoOOR71itZuX8lwNsRdL2ZkoFzIf/n85Nny', '12345123451234', '1 rue 349 opop', '45673', 'Rouen', NULL),
+(5, 'FloCorp', 'Flo@gmail.com', '$2y$10$vtIwDo6mUFur/8Z/M.KMr.IOSo5etwLE5oKo/EgciEiQbegr4hpkK', '99999999999999', '1 Karmine', '25478', 'Rouen', NULL);
 
 -- --------------------------------------------------------
 
@@ -122,7 +122,7 @@ CREATE TABLE IF NOT EXISTS `modedetransport` (
   `id_modedetransport` int NOT NULL AUTO_INCREMENT,
   `Type_modedetransport` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`id_modedetransport`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `modedetransport`
@@ -133,7 +133,8 @@ INSERT INTO `modedetransport` (`id_modedetransport`, `Type_modedetransport`) VAL
 (2, 'Marche à pied'),
 (3, 'Skate'),
 (4, 'trotinette'),
-(5, 'Roller');
+(5, 'Roller'),
+(6, 'Hoverboard');
 
 -- --------------------------------------------------------
 
@@ -143,16 +144,24 @@ INSERT INTO `modedetransport` (`id_modedetransport`, `Type_modedetransport`) VAL
 
 CREATE TABLE IF NOT EXISTS `trajet` (
   `id_trajet` int NOT NULL AUTO_INCREMENT,
-  `date_trajet` date DEFAULT NULL,
-  `distance_trajet` float(3,2) DEFAULT NULL,
-  `traveltime_trajet` time DEFAULT NULL,
+  `date_trajet` date NOT NULL,
+  `distance_trajet` float NOT NULL,
+  `traveltime_trajet` time NOT NULL,
   `image_trajet` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `id_modedetransport` int NOT NULL,
   `id_utilisateur` int NOT NULL,
   PRIMARY KEY (`id_trajet`),
   KEY `id_modedetransport` (`id_modedetransport`),
-  KEY `id_utilisateur` (`id_utilisateur`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  KEY `trajet_ibfk_2` (`id_utilisateur`)
+) ENGINE=InnoDB AUTO_INCREMENT=182 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `trajet`
+--
+
+INSERT INTO `trajet` (`id_trajet`, `date_trajet`, `distance_trajet`, `traveltime_trajet`, `image_trajet`, `id_modedetransport`, `id_utilisateur`) VALUES
+(166, '2024-02-01', 15245, '00:39:00', NULL, 5, 62),
+(167, '2024-02-06', 2, '01:00:00', NULL, 5, 63);
 
 -- --------------------------------------------------------
 
@@ -168,32 +177,26 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `birthdate_utilisateur` date DEFAULT NULL,
   `email_utilisateur` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `password_utilisateur` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `description_utilisateur` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `description_utilisateur` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `Image_utilisateur` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `ID_Entreprise` int NOT NULL,
   PRIMARY KEY (`id_utilisateur`),
   UNIQUE KEY `email_utilisateur` (`email_utilisateur`),
   UNIQUE KEY `password_utilisateur` (`password_utilisateur`),
   KEY `ID_Entreprise` (`ID_Entreprise`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=73 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `utilisateur`
 --
 
 INSERT INTO `utilisateur` (`id_utilisateur`, `firstname_utilisateur`, `lastname_utilisateur`, `nickname_utilisateur`, `birthdate_utilisateur`, `email_utilisateur`, `password_utilisateur`, `description_utilisateur`, `Image_utilisateur`, `ID_Entreprise`) VALUES
-(1, 'John', 'Smith', 'Ace', '1995-12-03', 'johnsmith123@gmail.com', 'DragonFire789!', 'Un aventurier intrépide', 'img.jpg', 1),
-(2, 'Emily', 'Johnson', 'Dottie', '1988-05-11', 'emilyjohnson334@gmail.com', 'SecretAgent007$', 'Une danseuse étoile', 'img.jpg', 1),
-(3, 'Michael', 'Davis', 'Sparky', '1979-05-21', 'michoudavis@yahoo.com', 'Wonderland123*', 'Un intellectuel érudit', 'img.jpg', 2),
-(4, 'Sophia', 'Anderson', 'Skipper', '2001-03-04', 'sophieanderson44@yahoo.com', 'GalaxyQuest456#', 'Une athlète olympique', 'img.jpg', 1),
-(5, 'William', 'Thompson', 'Buzzy', '1993-07-16', 'willthom@gmail.com', 'PirateAdventure!', 'Un champion de boxe', 'img.jpg', 2),
-(6, 'Olivia', 'Martinez', 'Scooter', '1965-12-29', 'oliviamartinez13@yahoo.com', 'MagicSpell789$', 'Une scientifique renommée', 'img.jpg', 2),
-(8, 'john', 'doe', 'ippo', '2024-12-19', 'johndoe@gmail.com', '$2y$10$ga8QL9QNc7z3fsElzfnjDuLTGTSVlu4umznBByvqTVHq68TueGtEK', NULL, NULL, 2),
-(20, 'sdfsd', 'sdfsd', 'sdfsdf', '2024-01-26', 'dfs@ippo.io', '$2y$10$/JaDo9P3f.HLCkoEu5gUjOQBtdbdHu9pM8x4A6sI2fSz3dOganjNC', NULL, NULL, 1),
-(21, 'pierre', 'Fil', 'baobab', '2024-01-19', 'pierre@gmail.com', '$2y$10$XZbsn1heDZiRANm/ZtLzKOv7gYUcumaGoouSGKnXT4zwylvGGJxnm', NULL, NULL, 1),
-(22, 'bryan', 'festin', 'baobaba', '2024-01-17', 'baobab@live.fr', '$2y$10$7WyzCmoxdjlfRZUJ6ORc1uz2404YvA4hju0k5sv/dBD./IgjpeaF.', NULL, NULL, 1),
-(24, 'Jean', 'franklin', 'yuyu', '2023-11-15', 'frankline@yahoo.com', '$2y$10$esrk/7LLRkxhcI.Rx6R/UODEbdLG.IMDRcRad6rZxoR0hMqgG6MGK', NULL, NULL, 2),
-(26, 'pierre', 'sdfsd', 'sdfsdf', '2024-01-09', 'tutu@live.fr', '$2y$10$bhmZMfC8s4z/zbdu1Ta9UO6habjjmpIleI/JHveSjk.2bxIz9n4ua', NULL, NULL, 1);
+(61, 'jotaro', 'jo', 'josukebestjojo', '2024-02-08', 'jojojo@gmail.com', '$2y$10$LZmyU2PNYD3Crt77Unvm5.uBZkweLYDQgx5rZYe3X2CTHne1YhsZG', NULL, NULL, 1),
+(62, 'HELENE', 'Poirier-Halley', 'LNWarrior', '1981-10-25', 'poirier.helene@outlook.fr', '$2y$10$QXrX4Gdxl9i807TzzLiI8uttO6sWZBxgA7dR9C5guLJ0jAW7L8LP.', NULL, NULL, 2),
+(63, 'John', 'LENNON', 'beatles', '2024-02-06', 'beatle@mail.fr', '$2y$10$UWN539QZ6TUByQlrk4d3KuyPTZx9Lk2xQwTUTM0fdrH/A/8t8GyuC', NULL, NULL, 1),
+(64, 'Florian', 'Malet', 'Zake', '2024-02-05', 'stephe76bis4@outlook.fr', '$2y$10$vBnnadx8LIXp.lKKJXVENOXGUcv8rLbHvhHeNI7W352DAHK8GEvzW', NULL, NULL, 1),
+(70, 'Bryan', 'Festin', 'Baobab', '2023-01-13', 'festinb@gmail.com', '$2y$10$PsqDblLG/icJXp9m5k6.iO2TRZ3jtWUNu7V4xK/8X7rplMrnTwYHa', 'lkdsjlksjdlfds', 'default.png', 3),
+(72, 'test', 'test', 'test', '2021-01-13', 'test@gmail.com', '$2y$10$cNvk5SuKJunRZTJBdjroLeLgX9OPsQNnW6dbGw5ks.ssROEZV1z/C', '', 'default.png', 5);
 
 --
 -- Contraintes pour les tables déchargées
@@ -217,7 +220,7 @@ ALTER TABLE `challenge_possede_mode_de_transport`
 --
 ALTER TABLE `trajet`
   ADD CONSTRAINT `trajet_ibfk_1` FOREIGN KEY (`id_modedetransport`) REFERENCES `modedetransport` (`id_modedetransport`),
-  ADD CONSTRAINT `trajet_ibfk_2` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id_utilisateur`);
+  ADD CONSTRAINT `trajet_ibfk_2` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id_utilisateur`) ON DELETE CASCADE ON UPDATE RESTRICT;
 
 --
 -- Contraintes pour la table `utilisateur`
